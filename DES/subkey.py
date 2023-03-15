a04ee6361e486110d323c37fa7784e725fa9739b
Laurie Luo <laurieluo2003@gmail.com>
2023-03-15 21:10:24 +0800

    renew code

--- DES/subkey.py
+++ DES/subkey.py
@@ -5,25 +5,24 @@ def gen_rand_key():
     return ''.join([chr(random.randint(0, 1)+ord('0')) for i in range(64)])
 
 class SubKey:
-    def __init__(self, key):
-        self.PC_1 = [57,49,41,33,25,17, 9, 1,\
-                     58,50,42,34,26,18,10, 2,\
-                     59,51,43,35,27,19,11, 3,\
-                     60,52,44,36,63,55,47,39,\
-                     31,23,15, 7,62,54,46,38,\
-                     30,22,14, 6,61,53,45,37,\
-                     29,21,13, 5,28,20,12, 4]
-        self.PC_2 = [14,17,11,24, 1, 5, 3,28,\
-                     15, 6,21,10,23,19,12, 4,\
-                     26, 8,16, 7,27,20,13, 2,\
-                     41,52,31,37,47,55,30,40,\
-                     51,45,33,48,44,49,39,56,\
-                     34,53,46,42,50,36,29,32]
+    PC_1 = [57,49,41,33,25,17, 9, 1,\
+            58,50,42,34,26,18,10, 2,\
+            59,51,43,35,27,19,11, 3,\
+            60,52,44,36,63,55,47,39,\
+            31,23,15, 7,62,54,46,38,\
+            30,22,14, 6,61,53,45,37,\
+            29,21,13, 5,28,20,12, 4]
+    PC_2 = [14,17,11,24, 1, 5, 3,28,\
+            15, 6,21,10,23,19,12, 4,\
+            26, 8,16, 7,27,20,13, 2,\
+            41,52,31,37,47,55,30,40,\
+            51,45,33,48,44,49,39,56,\
+            34,53,46,42,50,36,29,32]
 
-        #permuted choice one(PC-1)
-        #parameter 'key' is a string as default
+    #parameter 'key' is a string as default
+    def __init__(self, key):
         original_key_list = list(key)
-        self.round_0 = [ord(original_key_list[i-1])-ord('0') for i in self.PC_1]
+        self.round_0 = [ord(original_key_list[i-1])-ord('0') for i in PC_1]
 
     #i index from 1 to 16
     def get_subkey(self, i):
@@ -32,7 +31,7 @@ class SubKey:
         Ci = C0[move[i-1]:] + C0[:move[i-1]]
         Di = D0[move[i-1]:] + D0[:move[i-1]]
         sum = Ci + Di
-        return [sum[i-1] for i in self.PC_2]
+        return [sum[i-1] for i in PC_2]
 
 def bit_diff(num_DES):
     diff_num = []
